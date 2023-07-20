789ec777eec846cca018de54a4a4c5152364d3d4
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2023-07-20 22:49:03 +0900

    로그인 처리 - 서블린 HTTP 세션2

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -57,7 +58,7 @@ public class HomeController {
 
 
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model){
 
         // 세션이 없으면 홈으로
@@ -74,6 +75,18 @@ public class HomeController {
         }
 
         // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
+                                        Member loginMember,
+                                    Model model){
+        if (loginMember == null){
+            return "home";
+        }
+
         model.addAttribute("member", loginMember);
         return "loginHome";
     }

94e6000a68be888fa321393563c09f0e1889734e
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2023-07-20 22:44:56 +0900

    로그인 처리 - 서블린 HTTP 세션

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -41,17 +43,38 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
-    public String homeLoginV2(HttpServletRequest request, Model model){
+//    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+        if (member == null) {
+            return "home";
+        }
+        //로그인
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
 
-        // 세션관리자에 저장되 회원 정보 조회
-        Member member = (Member) sessionManager.getSession(request);
-        if (member == null){
+
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model){
+
+        // 세션이 없으면 홈으로
+        HttpSession session = request.getSession(false);
+        if (session == null){
             return "home";
         }
 
-        // 로그인
-        model.addAttribute("member", member);
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home으로
+        if (loginMember == null){
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
